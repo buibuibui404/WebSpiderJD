e386f80c05ded6d8f3b0079e6e1409b9e693cf52
tluo9434 <tluo9434@sydney.edu.au>
2019-10-30 07:28:23 +1100

    done some

--- test.py
+++ test.py
@@ -1,19 +1,73 @@
 import urllib.request
-import string
-from urllib.parse import quote
+import re
+# import string
+# from urllib.parse import quote
 
 # url = quote(url, safe = string.printable)
 # response = urllib.request.urlopen(url)
 # result = response.read().decode('utf-8')
 # print(result)
 
+    # f = open("test.txt","w") # create a new file
+    # f.write(str(html1, 'utf-8')) # put html to the new file
+    # f = open("test.txt", "w")
+    # html2 = urllib.request.urlopen(req2).read()
+    # f.write(str( html2, 'utf-8' ))
 
 def craw(url):
+
     req = urllib.request.Request(url)
     req.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36")
-    html1 = urllib.request.urlopen(req).read()
-    f = open("test.txt","w") # create a new file
-    f.write(str(html1, 'utf-8')) # put html to the new file
+    html1 = urllib.request.urlopen(req).read().decode('utf-8')
 
-url = r'https://search.jd.com/search?keyword=%E4%B8%89%E6%98%9F%E6%89%8B%E6%9C%BA&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&bs=1&wq=%E4%B8%89%E6%98%9F%E6%89%8B%E6%9C%BA&ev=exbrand_%E4%B8%89%E6%98%9F%EF%BC%88SAMSUNG%EF%BC%89%5E&cid3=655#J_searchWrap'
-craw(url)
+    result1 = re.compile(r'//item.*html').findall(html1)
+    result1 = list(set(result1))
+
+    result = []
+    for ele in result1:
+        try:
+            ls = ["https:",ele]
+            dict1 = {}
+            tempURL = "".join(ls)
+            tpR2 = urllib.request.Request(tempURL)
+            tpR2.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36")
+            html2 = urllib.request.urlopen(tpR2).read().decode('GBK')
+
+            n = re.search(r"商品名称：.*<", html2).group(0)
+            n = n[5:-1]
+
+            memo = re.search(r"运行内存：.*<", html2).group(0)
+            memo = memo[5:-1]
+
+            battery = re.search(r"电池容量（mAh）：.*<", html2).group(0)
+            battery = battery[10:-1]
+
+            color = re.search(r"机身颜色：.*<", html2).group(0)
+            color = color[5:-1]
+
+            front = re.search(r"前摄主摄像素：.*<", html2).group(0)
+            front = front[7:-1]
+
+            back = re.search(r"后摄主摄像素：.*<", html2).group(0)
+            back = back[7:-1]
+
+            dict1["name"] = {n}
+            dict1["memo"] = {memo}
+            dict1["battery"] = {battery}
+            dict1["color"] = {color}
+            dict1["front"] = {front}
+            dict1["back"] = {back}
+            result.append(dict1)
+            print(dict1)
+        except:
+            pass
+
+
+
+# url = 'https://search.jd.com/search?keyword=%E4%B8%89%E6%98%9F%E6%89%8B%E6%9C%BA&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&bs=1&wq=%E4%B8%89%E6%98%9F%E6%89%8B%E6%9C%BA&ev=exbrand_%E4%B8%89%E6%98%9F%EF%BC%88SAMSUNG%EF%BC%89%5E&cid3=655#J_searchWrap'
+url = "https://search.jd.com/search?keyword=%E4%B8%89%E6%98%9F%E6%89%8B%E6%9C%BA&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&bs=1&suggest=1.his.0.0&cid2=653&cid3=655&ev=exbrand_%E4%B8%89%E6%98%9F%EF%BC%88SAMSUNG%EF%BC%89%5E&page=1&s=1&click=0"
+print(craw(url))
+
+    # req2 = urllib.request.Request(quote("https://item.jd.com/100009128964.html", safe = string.printable))
+    # req2.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36")
+    # html2 = urllib.request.urlopen(req2).read().decode('GBK')
