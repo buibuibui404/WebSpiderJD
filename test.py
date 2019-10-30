c104d6297d967f8b82b01f7d3eeb56cb756f349b
tluo9434 <tluo9434@sydney.edu.au>
2019-10-30 11:28:43 +1100

    try to add next 30 items

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import urllib.request
 import re
+import json
+import csv
 # import string
 # from urllib.parse import quote
 
@@ -56,37 +58,83 @@ class JDitem:
 
     def getValue(self):
         return self.values
+    
+    def findPrice(self):
+        price = None
+        id = self.findID()
+        url = 'http://p.3.cn/prices/mgets?skuIds=J_' + id + '&type=1'
+        price_json = json.load(urllib.request.urlopen(url))[0]
+        if price_json['p']:
+            price = price_json['p']
+        self.values["price"] = price
+
+    def findID(self):
+        id = re.search(r"商品编号：.*<", self.html).group(0)
+        id = id[5:-1]
+        return id
+        
+# def firstHtml():
+
+
+# def lastHtml():
+#     a = time.time()
+#     b = '%.5f'%a
+#     url = 'https://search.jd.com/s_new.php?keyword=%E4%B8%89%E6%98%9F%E6%89%8B%E6%9C%BA&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&bs=1&suggest=1.his.0.0&cid2=653&cid3=655&ev=exbrand_%E4%B8%89%E6%98%9F%EF%BC%88SAMSUNG%EF%BC%89%5E&page={}&s={}&scrolling=y&log_id='+b
 
 
 
 def craw():
     i = 1
-    url = "https://search.jd.com/search?keyword=%E4%B8%89%E6%98%9F%E6%89%8B%E6%9C%BA&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&bs=1&suggest=1.his.0.0&cid2=653&cid3=655&ev=exbrand_%E4%B8%89%E6%98%9F%EF%BC%88SAMSUNG%EF%BC%89%5E&page={}&s=1&click=0".format(i)
-    
-    while i < 10:
-        req = urllib.request.Request(url)
-        req.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36")
-        html1 = urllib.request.urlopen(req).read().decode('utf-8')
 
-        result1 = re.compile(r'//item.*html').findall(html1)
-        result1 = list(set(result1))
+    a = time.time()
+    b = '%.5f'%a
+    url1 = "https://search.jd.com/search?keyword=%E4%B8%89%E6%98%9F%E6%89%8B%E6%9C%BA&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&bs=1&suggest=1.his.0.0&cid2=653&cid3=655&ev=exbrand_%E4%B8%89%E6%98%9F%EF%BC%88SAMSUNG%EF%BC%89%5E&page={}&s=1&click=0".format(i)
+    url2 = 'https://search.jd.com/s_new.php?keyword=%E4%B8%89%E6%98%9F%E6%89%8B%E6%9C%BA&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&bs=1&suggest=1.his.0.0&cid2=653&cid3=655&ev=exbrand_%E4%B8%89%E6%98%9F%EF%BC%88SAMSUNG%EF%BC%89%5E&page={}&s={}&scrolling=y&log_id='+b
+    result = []
 
-        result = []
-        for ele in result1:
-            ls = ["https:",ele]
-            dict1 = {}
-            tempURL = "".join(ls)
-            item = JDitem(tempURL)
-            item.findBack()
-            item.findBattery()
-            item.findColor()
-            item.findFront()
-            item.findMemo()
-            item.findName()
-            print(item.getValue())
+    try:
+        while i == 1:
+            req = urllib.request.Request(url1)
+            req.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36")
+            html1 = urllib.request.urlopen(req).read().decode('utf-8')
 
-        i += 2
+            result1 = re.compile(r'//item.*html').findall(html1)
+            result1 = list(set(result1))
 
+            print(result1)
+            for ele in result1:
+                try:
+                    ls = ["https:",ele]
+                    dict1 = {}
+                    tempURL = "".join(ls)
 
-craw()
+                    item = JDitem(tempURL)
+                    item.findBack()
+                    item.findBattery()
+                    item.findColor()
+                    item.findFront()
+                    item.findMemo()
+                    item.findName()
+                    item.findPrice()
+                    result.append(item.getValue())
+                    print(item.getValue())
+                except:
+                    pass
+            i += 2
+    except:
+        pass
+    print("!!")
+    return result
+
+def creatCSV(ls):
+    fileHeader = ["name", "price", "memo", "battery", "color", "back", "front"]
+    f = open("test.csv", "w", encoding='utf-8-sig')
+    writer = csv.DictWriter(f, fileHeader)
+    writer.writeheader()
+    for ele in ls:
+        print(ele)
+        writer.writerow(ele)
+
+if __name__ == "__main__":
+    creatCSV(craw())
 
